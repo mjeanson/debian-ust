6e2c8bbaf2f5f9109d43595e374c43c05938616d
Jon Bernard <jbernard@debian.org>
2011-07-16 18:42:56 -0400

    New upstream version 0.14

--- include/ust/stringify.h
+++ include/ust/stringify.h
@@ -1,12 +1,12 @@
+#ifndef __LINUX_STRINGIFY_H
+#define __LINUX_STRINGIFY_H
 /*
- * simple.h
- *
- * Copyright (C) 2009 - Pierre-Marc Fournier (pierre-marc dot fournier at polymtl dot ca)
+ * Copyright (C) 2009 Zhaolei <zhaolei@cn.fujitsu.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * License as published by the Free Software Foundation;
+ * version 2.1 of the License.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -18,14 +18,12 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
  */
 
-#ifndef KCOMPAT_SIMPLE_H
-#define KCOMPAT_SIMPLE_H
+/* Indirect stringification.  Doing two levels allows the parameter to be a
+ * macro itself.  For example, compile with -DFOO=bar, __stringify(FOO)
+ * converts to "bar".
+ */
 
-/* libkcompat: from rcupdate.h */
+#define __stringify_1(x...)	#x
+#define __stringify(x...)	__stringify_1(x)
 
-struct rcu_head {
-	struct rcu_head *next;
-	void (*func)(struct rcu_head *head);
-};
-
-#endif /* KCOMPAT_SIMPLE_H */
+#endif	/* !__LINUX_STRINGIFY_H */
